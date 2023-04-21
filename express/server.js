c429eb494b609bdc7176233f926ca7a78224c82a
the-me-0 <noah.philippe@etudiant.univ-lr.fr>
2023-04-21 16:47:29 +0200

    add(front) : HMR working, had to separate nginx from vite and set up compose, nginx

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-connect('mongodb://mongodb/tars_mail_db', { useNewUrlParser: true, useUnifiedTopology: true })
+connect('mongodb://mongo/tars_mail_db', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
 
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-const port = process.env.PORT || 3000
+const port = 4000
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
