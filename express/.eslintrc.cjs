46b1638afd6ad2b0506595576618b9e509044f5b
the-me-0 <noah.philippe@etudiant.univ-lr.fr>
2023-04-21 13:20:37 +0200

    chore(all) : added eslint to the project

--- express/.eslintrc.cjs
+++ express/.eslintrc.cjs
@@ -3,19 +3,13 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
+  extends: 'standard-with-typescript',
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
   rules: {
   }
 }
--- express/server.js
+++ express/server.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import { connect } from 'mongoose';
-import bodyParser from 'body-parser';
+import express from 'express'
+import { connect } from 'mongoose'
+import bodyParser from 'body-parser'
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 connect('mongodb://mongodb/tars_mail_db', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
